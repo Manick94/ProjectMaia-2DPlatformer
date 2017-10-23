f2ba462776edc6e603d0357527cb40fcd820529e
serity93 <sean.ricardo.tyler@gmail.com>
2017-10-23 16:18:02 -0700

    Added state interface and machine

---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+
+	private IState currentState;
+	private IState previousState;
+
+	public void ChangeState (IState newState)
+	{
+		if (currentState != null) {
+			currentState.Exit ();
+		}
+
+		previousState = currentState;
+		currentState = newState;
+
+		currentState.Enter ();
+	}
+
+	public void Update ()
+	{
+		if (currentState != null) {
+			currentState.Update ();
+		}
+	}
+
+	public void SwitchToPreviousState ()
+	{
+		if (currentState != null && previousState != null) {
+			currentState.Exit ();
+			currentState = previousState;
+			currentState.Enter ();
+		}
+	}
+}
